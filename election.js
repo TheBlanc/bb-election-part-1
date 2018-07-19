4fd20c5356480eac73516b9743f46e6c966a76c2
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-18 23:51:46 -0400

    Add election results button that triggers AJAX call.

--- election.js
+++ election.js
@@ -1,5 +1,32 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
+  var resultButton = document.querySelector('#checkResult');
+  var result = document.querySelector('#result');
+
+
+  resultButton.addEventListener('click', function() {
+    $.ajax({
+      url: 'https://bb-election-api.herokuapp.com',
+      method: 'GET',
+      dataType: 'json'
+    }).done( function(responseData) {
+      result.innerHTML = "";
+      responseData.candidates.forEach( function(cand) {
+        var name = document.createElement('h2');
+        var votes = document.createElement('p');
+        name.innerHTML = cand.name;
+        votes.innerHTML = "Votes: " + cand.votes;
+        result.appendChild(name);
+        result.appendChild(votes);
+      });
+    }).fail( function(responseData) {
+      console.log("ERROR");
+      result.innerHTML = "";
+      var error = document.createElement('h2');
+      error.innerHTML = "Whoops, something went wrong...";
+      result.appendChild(error);
+    });
+  });
+
 
 });
